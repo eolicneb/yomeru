ca5cb261d2ecce63d7f7a8260bd6c0487d7ebc56
Nicolas Bueno <eoneb@hotmail.com>
2021-04-09 01:24:23 -0300

    output to html file

--- mitoushi.py
+++ mitoushi.py
@@ -1,230 +1,43 @@
-from copy import deepcopy
-from math import ceil
-
-from language.tsubu import Tsubu
-
-""" 見通し: outlook, view
-
-The length of lines for regular romaji characters 
-and those for kanjis and kanas are in ratio 39:24.
-IOW, each kana and kanki "measures" like 1.625
-romaji characters.
-"""
+def new_cell(row, html):
+    cell = html.new_tag('td')
+    row.append(cell)
+    return cell
 
 
-KANJI_LENGTH_FACTOR = 1.625
+def new_row(table, html):
+    row = html.new_tag('tr')
+    table.append(row)
+    return row
 
 
-class EOL(Exception):
-    pass
+def insert_tsubu(tsubu, cell, html):
+    new_table = html.new_tag('table')
+    new_table['imi'] = tsubu.imi
+    cell.append(new_table)
 
-
-class KaiteKanji:
-    L_FACTOR = KANJI_LENGTH_FACTOR
-
-    def __init__(self, kao, total_len=None):
-        self.kao = str(kao)
-        self._kao_len = (len(kao)
-                         if isinstance(kao, self.__class__)
-                         else len(kao) * self.L_FACTOR)
-        self._total_len = total_len
-        self._margin_space = None
-
-    @property
-    def total_len(self):
-        if self._total_len is None:
-            self._total_len = self.kao_len
-        return self._total_len
-
-    @total_len.setter
-    def total_len(self, total_len):
-        assert total_len >= self.kao_len, \
-            (f"The total length {total_len} must not be"
-             f" smaller than the text length {self.kao_len}")
-        self._total_len = total_len
-        self.make_margins()
-
-    def make_margins(self):
-        extra_space = (self.total_len - self.kao_len) / 2
-        self._margin_space = (round(extra_space),
-                              ceil(extra_space))
-
-    def add_space(self, extra_space):
-        self.total_len += extra_space
-
-    @property
-    def kao_len(self):
-        if self._kao_len is None:
-            self._kao_len = len(self.kao) * self.L_FACTOR
-        return self._kao_len
-
-    def __len__(self):
-        return ceil(self.total_len)
-
-    def __str__(self):
-        if self._margin_space is None:
-            self.make_margins()
-        return (" " * self._margin_space[0]
-                + str(self.kao)
-                + " " * self._margin_space[1])
-
-    def __add__(self, other):
-        new_len, new_str = self.add_core(other)
-        new = self.__class__(new_str, new_len)
-        new._kao_len = new_len
-        return new
-
-    def __iadd__(self, other):
-        new_len, new_str = self.add_core(other)
-        self.kao = new_str
-        self._kao_len = self._total_len = new_len
-        self._margin_space = (0, 0)
-        return self
-
-    def add_core(self, other):
-        if not isinstance(other, self.__class__):
-            new_len = self.total_len + len(other) * self.L_FACTOR
-        else:
-            new_len = self.total_len + other.total_len
-        new_str = str(self) + str(other)
-        return new_len, new_str
-
-
-class KaketaiTsubu:
-    L_FACTOR = KANJI_LENGTH_FACTOR
-
-    def __init__(self, tsubu: [Tsubu]):
-        self.tsubu = tsubu
-        self.tsubu._max_len = None
-
-    @property
-    def max_len(self):
-        if self.tsubu._max_len is None:
-            self.tsubu._max_len = max(
-                self._uchi_len, len(self.tsubu.imi))
-        if len(self.tsubu.imi) > len(self.tsubu.kao)*self.L_FACTOR:
-            print(self.tsubu.kao, self.tsubu.imi)
-        return self.tsubu._max_len
-
-    @property
-    def _uchi_len(self):
-        if not hasattr(self.tsubu, "_uchiryuu"):
-            return self._ryuushi_len(self.tsubu)
-
-        running_len = 0
-        for shi in self.tsubu._uchiryuu:
-            if hasattr(shi, "_uchiryuu"):
-                running_len += self.__class__(shi).max_len
-            else:
-                running_len += self._ryuushi_len(shi)
-        return running_len
-
-    @classmethod
-    def _ryuushi_len(cls, shi):
-        len_onsei = (len(shi.onsei) * cls.L_FACTOR
-                     if shi.onsei else 0)
-        len_kao = len(shi.kao) * cls.L_FACTOR
-        return ceil(max(len_onsei, len_kao, len(shi.imi)))
-
-
-class KanjiRetsu:
-    L_FACTOR = KANJI_LENGTH_FACTOR
-
-    def __init__(self, max_len=None, len_margin=0.7):
-        self._kao = ""
-        self.len = 0
-        self.max_len = max_len
-        self.MARGIN = len_margin
-
-    @property
-    def kao(self):
-        return self._kao
-
-    def extend(self, other):
-        assert isinstance(other, self.__class__), \
-            f"{self.__class__} expected, got {other.__class__}"
-        if (self.max_len != other.max_len
-                or self.MARGIN != other.MARGIN):
-            raise ValueError("Only objects with same 'max_len' and "
-                             "'len_margin' attributes can be extended.")
-        self._kao += other._kao
-        self.len += other.len
-
-    def append(self, kanji_text):
-        assert isinstance(kanji_text, str), f"Only str can be appended, not {type(kanji_text)}"
-        if not kanji_text:
-            return
-        text_len = self.L_FACTOR * len(kanji_text)
-        self.check_new_len(text_len)
-        self.len += text_len
-        self.check_remainder()
-
-    def check_remainder(self):
-        if 1 + int(self.len) - self.len > 1:
-            self._kao += " "
-            self.len += 1
-
-    def check_new_len(self, text_len):
-        if not self.max_len:
-            return
-        if self.len + text_len > self.max_len + self.MARGIN:
-            raise EOL
-
-    def __add__(self, other):
-        temp_third = deepcopy(self)
-        return temp_third.extend(other)
-
-    def __iadd__(self, other):
-        self.extend(other)
-
-
-class Retsu:  # 列: column
-    def __init__(self, max_length=None):
-        self.onsei = ""
-        self.kao = ""
-        self.imi = []
-        self.max_length = None
-        self.running_length = 0
-        self.onsei_len_flag = True
-        self.kao_len_flag = True
-
-    def onsei_len(self, kan_text, flag):
-        self.k_len_flag = not self.k_len_flag
-
-
-def kake(retsu: Retsu):
-    print(retsu.onsei)
-    print(retsu.kao)
-    print(retsu.imi)
-
-
-def escribir(kanas):
-    retsu = Retsu()
-    for k in kanas:
-        retsu.add(k)
-    return retsu
-
-
-to_dict = {'kao': "", 'onsei': "", 'imi': []}
-
-
-def len2len(tsubu: Tsubu, running_dict):
-    structure = {'tsubu': tsubu, 'len': 0}
-    if hasattr(tsubu, "_uchiryuu"):
-        level, running_retsu = 0, running_dict['kao'][-1]
+    if hasattr(tsubu, '_uchiryuu'):
+        row = new_row(new_table, html)
         for shi in tsubu._uchiryuu:
-            sub_level, sub_kao = len2len(shi)
-            level = max(level, sub_level)
-            try:
-                running_retsu += sub_kao
-            except EOL:
-                running_retsu = KanjiRetsu(max_len=running_retsu.max_len,
-                                           len_margin=running_retsu.MARGIN)
-                running_dict['kao'].append(running_retsu)
-                running_retsu += sub_kao
-        level += 1
+            insert_tsubu(shi, new_cell(row, html), html)
+
+        colspan = len(tsubu._uchiryuu)
+
     else:
-        level = 0
-    print(f"kao: {tsubu.kao}...\nimi: {tsubu.imi}'''")
-    print(f" - {len(tsubu)} vs imi {len(tsubu.imi)}")
-    return level, running_retsu
+        onsei_row = new_row(new_table, html)
+        on_cell = new_cell(onsei_row, html)
+        on_cell.string = tsubu.onsei if tsubu.onsei else ""
+        on_cell['class'] = "onsei"
+
+        kao_row = new_row(new_table, html)
+        kao_cell = new_cell(kao_row, html)
+        kao_cell.string = tsubu.kao
+        kao_cell['class'] = "kao"
+
+        colspan = 1
+
+    if tsubu.imi:
+        imi_row = new_row(new_table, html)
+        imi_cell = new_cell(imi_row, html)
+        imi_cell.string = tsubu.imi
+        imi_cell['class'] = "imi"
+        imi_cell['colspan'] = colspan
--- tests/test_mitoushi.py
+++ tests/test_mitoushi.py
@@ -1,92 +1,9 @@
 import pytest
-from math import ceil
 
-from language.tsubu import Tsubu
-from mitoushi import KaketaiTsubu, KaiteKanji
+import bs4
 
-from tests import kanji_text as test_text
-
-
-def len2len(tsubu: Tsubu):
-    display = {'kao': "", 'onsei': "", 'imi': []}
-    if hasattr(tsubu, "_uchiryuu"):
-        for shi in tsubu._uchiryuu:
-            len2len(shi)
-    else:
-        print(" -o-")
-    shi = KaketaiTsubu(tsubu)
-    print(f"onsei: {kanji_line(str(tsubu.onsei), shi.max_len)}")
-    print(f"kao:   {kanji_line(str(tsubu.kao), shi.max_len)}")
-    print(f"imi:   {plain_line(str(tsubu.imi), shi.max_len)}")
-    print(f"max_len: {shi.max_len}")
-
-
-def kaite_display(display_dict):
-    print(display_dict['onsei'])
-    print(display_dict['kao'])
-    for imi in display_dict['imi']:
-        print(imi)
-
-
-def kaite(tsubu: Tsubu):
-    display = {'kao': KaiteKanji(""),
-               'onsei': KaiteKanji(""),
-               'imi': []}
-    total_len = KaketaiTsubu(tsubu).max_len
-
-    if hasattr(tsubu, "_uchiryuu"):
-
-        extra_len = max(len(tsubu.imi) - total_len, 0) / len(tsubu._uchiryuu)
-        running_imi, running_len = [], []
-        for shi in tsubu._uchiryuu:
-            shi_display = kaite(shi)
-            for key in ('kao', 'onsei'):
-                shi_display[key].add_space(extra_len)
-                display[key] += shi_display[key]
-            running_imi.append(shi_display['imi'])
-            running_len.append(shi_display['len'])
-
-        level = 0
-        while True:
-            if not any(len(imi) > level for imi in running_imi):
-                break
-            display['imi'].append(
-                "|".join(plain_line((imi[level]
-                                     if len(imi) > level
-                                     else ""),
-                                    imi_len + extra_len)
-                         for imi, imi_len in zip(running_imi, running_len)))
-            level += 1
-
-    else:
-
-        display['kao'] = KaiteKanji(tsubu.kao, total_len)
-        display['onsei'] = KaiteKanji(
-            tsubu.onsei if tsubu.onsei is not None else "",
-            total_len)
-
-    if tsubu.imi:
-        display['imi'].append(plain_line(tsubu.imi,
-                                         max(len(tsubu.imi), total_len)))
-    display['len'] = max(total_len, len(tsubu.imi))
-    print("-" * round(total_len))
-    kaite_display(display)
-    return display
-
-
-def kanji_line(kanji_text, length):
-    l_factor = 1.625
-    len_kanji_text = ceil(len(kanji_text) * l_factor)
-    return extend_line(kanji_text, (length - len_kanji_text))
-
-
-def plain_line(plain_text, length):
-    return extend_line(plain_text, (length - len(plain_text)))
-
-
-def extend_line(text, extra_length):
-    len_space = extra_length / 2.0
-    return " " * round(len_space) + text + " " * ceil(len_space)
+from mitoushi import insert_tsubu
+import tests.kanji_text as test_text
 
 
 @pytest.fixture
@@ -94,16 +11,9 @@ def text():
     return test_text.text()
 
 
-@pytest.mark.skip
-def test_kanji_retsu(text):
-    print()
-    len2len(text)
-
-
-def test_kaite(text):
-    print()
-    display = kaite(text)
-    print(display['onsei'])
-    print(display['kao'])
-    for imi in display['imi']:
-        print(imi)
+def test_table(text):
+    doc = bs4.BeautifulSoup('<!DOCTYPE html><header><meta charset="UTF-8"><style/></header><body><table><tr><td>')
+    doc.header.style.string = open("test.css", "r").read()
+    insert_tsubu(text, doc.tr.td, doc)
+    with open("test.html", "w", encoding="utf8") as f:
+        f.write(doc.prettify())
